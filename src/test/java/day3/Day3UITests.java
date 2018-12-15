9a6ace2579365e4ba46ce4ebce6e14f623331a4a
ab054 <alfamiliya@gmail.com>
2018-12-15 13:15:00 -0800

    UI test added

--- src/test/java/day3/Day3UITests.java
+++ src/test/java/day3/Day3UITests.java
@@ -1,11 +1,33 @@
 package day3;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Day3UITests {
 
+    WebDriver driver;
+
     @Test
     public void test001() throws Exception {
+        String path = System.getProperty("user.dir") + "/src/test/resources/geckodriver";
+        System.setProperty("webdriver.gecko.driver", path);
 
+        driver = new FirefoxDriver();
+
+        openMainPage();
+        typeQuery();
+       // submitQuery();
+       // checkingResult();
+
+    }
+
+    private void typeQuery() {
+        driver.findElement(By.cssSelector(".gLFyf"));
+    }
+
+    private void openMainPage() {
+        driver.get("http://google.com");
     }
 }
